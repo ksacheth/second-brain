d416b16504a9af897ad88827b3ab01f8528171f5
ksacheth <tech.sachethkoushal@gmail.com>
2025-12-17 07:37:52 +0530

    Added .env

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,2 @@
-export const JWT_PASSWORD = "randome";
+export const JWT_PASSWORD = process.env.JWT_PASSWORD;
+export const MONGODB_URL = process.env.MONGODB_URL;
--- backend/db.js
+++ backend/db.js
@@ -1,35 +1,35 @@
 import mongoose from "mongoose";
+import { MONGODB_URL } from "./config.js";
 
-mongoose.connect("mongodb://localhost:27017/brainly");
+mongoose.connect(MONGODB_URL);
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const User = new Schema ({
-    name : String,
-    // email: {type: String , required: true, unique: true},
-    username: {type: String , required: true, unique: true},
-    password: {type: String, required:true}
+const User = new Schema({
+  name: String,
+  // email: {type: String , required: true, unique: true},
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
 const contentSchema = new Schema({
-    userId: {type: ObjectId, ref: 'User', required: true},
-    title: {type: String, required:true},
-    link: String,
-    tags: [{type: ObjectId, ref: 'Tag'}],
-    type: [{type: String}]
+  userId: { type: ObjectId, ref: "User", required: true },
+  title: { type: String, required: true },
+  link: String,
+  tags: [{ type: ObjectId, ref: "Tag" }],
+  type: [{ type: String }],
 });
 
 const linkSchema = new Schema({
-    hash: {type: String , required: true},
-    userId: {type: ObjectId, ref: 'User', required: true}
+  hash: { type: String, required: true },
+  userId: { type: ObjectId, ref: "User", required: true },
 });
 
 const tagsSchema = new Schema({
-    title: {type: String, required: true},
-    userId: {type: ObjectId, ref: 'User', required: true}
+  title: { type: String, required: true },
+  userId: { type: ObjectId, ref: "User", required: true },
 });
-
 
 export const UserModel = mongoose.model("User", User);
 export const ContentModel = mongoose.model("content", contentSchema);
